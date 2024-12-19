29474dc0e8129e31f99fec0b07fb33ef0448178d
github-action-benchmark <github@users.noreply.github.com>
2024-12-19 00:39:02 +0000

    add Benchmark (cargo) benchmark result for 478b1f90964135e690cc47f0f1784469c94ffb3b

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1734568742481,
+  "repoUrl": "https://github.com/commonwarexyz/monorepo",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "commonwarexyz",
+            "username": "commonwarexyz"
+          },
+          "committer": {
+            "name": "commonwarexyz",
+            "username": "commonwarexyz"
+          },
+          "id": "478b1f90964135e690cc47f0f1784469c94ffb3b",
+          "message": "Publish benchmarks to /benchmarks repo",
+          "timestamp": "2024-12-18T20:28:22Z",
+          "url": "https://github.com/commonwarexyz/monorepo/pull/286/commits/478b1f90964135e690cc47f0f1784469c94ffb3b"
+        },
+        "date": 1734568742315,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "sender/1024",
+            "value": 2687,
+            "range": "± 39",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
