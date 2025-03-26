d0f2488b1aa8fe2223cf8b6c5aa695aaa93509dc
github-action-benchmark <github@users.noreply.github.com>
2025-03-26 12:00:24 +0000

    add commonware-stream (cargo) benchmark result for 863835f5d95b8d8a7d67c00b1a9885400fee87c3

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742990144633,
+  "lastUpdate": 1742990424617,
   "repoUrl": "https://github.com/commonwarexyz/monorepo",
   "entries": {
     "commonware-cryptography": [
@@ -69607,6 +69607,124 @@ window.BENCHMARK_DATA = {
             "name": "public_key::receiver_receive/len=67108864",
             "value": 178910411,
             "range": "± 644941",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Roberto Bayardo",
+            "username": "roberto-bayardo",
+            "email": "roberto@commonware.xyz"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "863835f5d95b8d8a7d67c00b1a9885400fee87c3",
+          "message": "[commonware-storage/mmr] Simplify & improve MMR pruning behavior (#643)",
+          "timestamp": "2025-03-25T22:56:46Z",
+          "url": "https://github.com/commonwarexyz/monorepo/commit/863835f5d95b8d8a7d67c00b1a9885400fee87c3"
+        },
+        "date": 1742990423886,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "public_key::sender_send/len=1024",
+            "value": 2710,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::sender_send/len=4096",
+            "value": 4981,
+            "range": "± 51",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::sender_send/len=16384",
+            "value": 14046,
+            "range": "± 53",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::sender_send/len=262144",
+            "value": 203875,
+            "range": "± 14477",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::sender_send/len=1048576",
+            "value": 803694,
+            "range": "± 101152",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::sender_send/len=4194304",
+            "value": 4425020,
+            "range": "± 38498",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::sender_send/len=16777216",
+            "value": 17207899,
+            "range": "± 83744",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::sender_send/len=67108864",
+            "value": 89274269,
+            "range": "± 240132",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::receiver_receive/len=1024",
+            "value": 4539,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::receiver_receive/len=4096",
+            "value": 11942,
+            "range": "± 1253",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::receiver_receive/len=16384",
+            "value": 41518,
+            "range": "± 501",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::receiver_receive/len=262144",
+            "value": 639837,
+            "range": "± 4917",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::receiver_receive/len=1048576",
+            "value": 2570039,
+            "range": "± 8466",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::receiver_receive/len=4194304",
+            "value": 10118273,
+            "range": "± 64656",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::receiver_receive/len=16777216",
+            "value": 43775238,
+            "range": "± 166631",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "public_key::receiver_receive/len=67108864",
+            "value": 176773243,
+            "range": "± 752484",
             "unit": "ns/iter"
           }
         ]
